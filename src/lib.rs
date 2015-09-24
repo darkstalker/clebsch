b1601d474dc0a13028c2e1b8760c159e1a2f25e7
darkstalker <slayerbeast@gmail.com>
2015-09-24 00:48:23 -0300

    use more ints

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@ use num::traits::{Zero, One, Signed, ToPrimitive};
 use num::bigint::{BigInt, ToBigInt};
 use num::rational::BigRational;
 
-fn factorial(nf: f32) -> BigInt
+fn factorial(n: i64) -> BigInt
 {
-    let n = nf as i64; // hack, let's do the cast here
     let mut acc = One::one();
     for i in 2 .. n + 1
     {
@@ -14,14 +13,14 @@ fn factorial(nf: f32) -> BigInt
     acc
 }
 
+fn factorial_f(n: f32) -> BigInt
+{
+    factorial(n as i64)
+}
+
 fn minus_one_pow(n: i64) -> i64
 {
     1 - n % 2 * 2
-}
-
-fn to_bigint(n: f32) -> BigInt
-{
-    (n as i64).to_bigint().unwrap()
 }
 
 // could optimize the type usage better if i knew what i'm doing
@@ -39,12 +38,12 @@ pub fn clebschgordansq(j1: f32, m1: f32, j2: f32, m2: f32, j12: f32, m12: f32) -
         return (0, Zero::zero())
     }
 
-    let mut c1 = kmin.trunc();
-    let mut c2 = (j1 + j2 - j12 - kmin).trunc();
-    let mut c3 = (j1 - m1 - kmin).trunc();
-    let mut c4 = (j2 + m2 - kmin).trunc();
-    let mut c5 = (j12 - j2 + m1 + kmin).trunc();
-    let mut c6 = (j12 - j1 - m2 + kmin).trunc();
+    let mut c1 = kmin as i64;
+    let mut c2 = (j1 + j2 - j12 - kmin) as i64;
+    let mut c3 = (j1 - m1 - kmin) as i64;
+    let mut c4 = (j2 + m2 - kmin) as i64;
+    let mut c5 = (j12 - j2 + m1 + kmin) as i64;
+    let mut c6 = (j12 - j1 - m2 + kmin) as i64;
     let mut c = BigRational::new(
         minus_one_pow(kmin as i64).to_bigint().unwrap(),
         factorial(c1) * factorial(c2) * factorial(c3) * factorial(c4) * factorial(c5) * factorial(c6)
@@ -53,16 +52,16 @@ pub fn clebschgordansq(j1: f32, m1: f32, j2: f32, m2: f32, j12: f32, m12: f32) -
 
     for _ in kmin as i64 .. kmax as i64  // k unused, removed the +1
     {
-        c1 += 1.;
-        c5 += 1.;
-        c6 += 1.;
+        c1 += 1;
+        c5 += 1;
+        c6 += 1;
         c = c * BigRational::new(
-            to_bigint(-c2 * c3 * c4),  // could overflow, don't know the size of values
-            to_bigint(c1 * c5 * c6)
+            (-c2 * c3 * c4).to_bigint().unwrap(),  // could overflow, don't know the size of values
+            (c1 * c5 * c6).to_bigint().unwrap()
         );
-        c2 -= 1.;
-        c3 -= 1.;
-        c4 -= 1.;
+        c2 -= 1;
+        c3 -= 1;
+        c4 -= 1;
         r = r + c.clone();  // ops::AddAssign is still on RFC
     }
 
@@ -70,17 +69,17 @@ pub fn clebschgordansq(j1: f32, m1: f32, j2: f32, m2: f32, j12: f32, m12: f32) -
     let rsq = r.clone() * r; // ugly, pow() not implemented for BigInt
 
     let val = BigRational::new(
-        to_bigint(2. * j12 + 1.) *
-        factorial(j12 + j1 - j2) *
-        factorial(j12 - j1 + j2) *
-        factorial(j1 + j2 - j12) *
-        factorial(j12 + m12) * // reordered things here because BigRational * BigInt isn't implemented
-        factorial(j12 - m12) *
-        factorial(j1 - m1) *
-        factorial(j1 + m1) *
-        factorial(j2 - m2) *
-        factorial(j2 + m2),
-        factorial(j1 + j2 + j12 + 1.)
+        ((2. * j12 + 1.) as i64).to_bigint().unwrap() *
+        factorial_f(j12 + j1 - j2) *
+        factorial_f(j12 - j1 + j2) *
+        factorial_f(j1 + j2 - j12) *
+        factorial_f(j12 + m12) * // reordered things here because BigRational * BigInt isn't implemented
+        factorial_f(j12 - m12) *
+        factorial_f(j1 - m1) *
+        factorial_f(j1 + m1) *
+        factorial_f(j2 - m2) *
+        factorial_f(j2 + m2),
+        factorial_f(j1 + j2 + j12 + 1.)
     ) * rsq;
 
     (sign, val)
